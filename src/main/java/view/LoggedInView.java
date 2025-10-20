7e1f91fa8bbef42fcce43ebab24c520f7567052e
kynmiiso <155723165+kynmiiso@users.noreply.github.com>
2025-10-20 15:48:49 -0400

    completed LoggedInView TODO

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -4,6 +4,7 @@ import interface_adapter.logged_in.ChangePasswordController;
 import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.logout.LogoutController;
+import use_case.login.LoginInputData;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
@@ -108,7 +109,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      * @param evt the ActionEvent to react to
      */
     public void actionPerformed(ActionEvent evt) {
-        // TODO: execute the logout use case through the Controller
+        logoutController.execute();
         System.out.println("Click " + evt.getActionCommand());
     }
 
@@ -140,6 +141,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
